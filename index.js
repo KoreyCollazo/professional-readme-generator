3004f953586d748b68b83028d98fe1143b31db5b
Korey Collazo <koreycollazo@Koreys-MacBook-Pro.local>
2023-01-02 13:35:17 -0500

    added function generate markdown for the readMe file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-
+// Questions for promt
 const questions = () => {
   return inquirer.prompt([
   {
